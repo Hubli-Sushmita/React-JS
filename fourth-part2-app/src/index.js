f1bd258a54cc41cebc945ca29d737872f2da7766
Hubli-Sushmita <hubli.s@northeastern.edu>
2024-11-20 15:02:31 -0500

    rendering react components way 2

--- fourth-part2-app/src/index.js
+++ fourth-part2-app/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <h1>Approach number 2 of rendering React componrnts in your web page</h1>
     <App />
   </React.StrictMode>
 );
